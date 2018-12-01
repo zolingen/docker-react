6c838e5da958f4b274d368d215e5d02d5fb72c8a
zolingen <zolingen@gmail.com>
2018-12-01 11:38:23 +0100

    tekst plus feature-branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload zegt JABBA the HUT.
+            Edit <code>src/App.js</code> and save to reload zegt JABBA the HUT -nu plus feature brach-
           </p>
           <a
             className="App-link"
